5d00b9a12d804650804d027dc2d037291d0a888a
DS <dsathyakumar@users.noreply.github.com>
2024-04-04 23:09:53 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import compressionMiddleware from "compression";
+import markoMiddleware from "@marko/express";
+import indexPage from "./pages/index";
+import iframePage from "./pages/iframe";
+
+const port = parseInt(process.env.PORT || 3000, 10);
+
+express()
+  .use(compressionMiddleware()) // Enable gzip compression for all HTTP responses.
+  .use("/assets", express.static("dist/assets")) // Serve assets generated from webpack.
+  .use(markoMiddleware()) // Enables res.marko.
+  .get("/", indexPage)
+  .get("/iframe", iframePage)
+  .listen(port, err => {
+    if (err) {
+      throw err;
+    }
+
+    if (port) {
+      console.log(`Listening on port ${port}`);
+    }
+  });
